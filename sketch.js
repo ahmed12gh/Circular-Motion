cdca195fd05c16e8ddd2e1f9faf2c0b4617668d6
ahmed12gh <ahmed.hashim2005@gmail.com>
2022-03-24 00:25:24 +0300

    sliders , non-uniform motion and more

--- sketch.js
+++ sketch.js
@@ -1,28 +1,50 @@
 
-let r = 250; 
+var Rslider = document.getElementById("R-slider")
+var velSlider = document.getElementById("vel-slider")
+
+var irr_check = document.getElementById("irregular")
+var At_s = document.getElementById("at-slider")
+
+let r = 150
+let safe_r = r + 80
+let A_c ;
+let A_t ; 
 let tri = 15 ; 
+let irregular_acc  = false; 
+
 class car { 
   constructor(x,y){
-    this.loc = createVector();
+    this.loc = createVector(x,y);
     this.vel = createVector(0,4);
     this.acc = createVector(0,1);
+
+
     this.maxspeed = 4; 
     this.maxforce = 0.1; 
-    
-    this.loc.x = x ; 
-    this.loc.y = y ; 
+    A_c = this.vel.magSq() / r
+  
   }
 
   update( ){
-    this.acc.setHeading(this.vel.heading() +HALF_PI );
-    this.acc.setMag(this.vel.magSq() / r);
+    if (irregular_acc == false){
+      this.acc.setHeading(this.vel.heading() + HALF_PI );
+      this.acc.setMag(this.vel.magSq() / r);  
+    }
+    else{ 
+      let mag  = Math.sqrt(Math.pow(A_c , 2)  + Math.pow(A_t,2))  ;
+      let theta = Math.atan2(A_c , A_t)
+      
+      this.acc.setMag(mag)
+      this.acc.setHeading(this.vel.heading()  + theta);
+    }
+    
+    
+    this.edge()
     this.vel.add(this.acc);
     this.vel.limit(this.maxspeed);
     this.loc.add(this.vel);
   }
 
-  seek(){
-  }
   applyforce(force){
     this.acc.add(force);
   }
@@ -35,12 +57,12 @@ class car {
    
     strokeWeight(2)
     stroke(140,30,10,160) 
-    line(0,0,this.acc.x * 1000, this.acc.y * 1000);
+    line(0,0,this.acc.x * 300 , this.acc.y * 300);
     stroke(10,30,160,160) 
-    line(0,0,this.vel.x * 100 , this.vel.y * 100);
+    line(0,0,this.vel.x * 30 , this.vel.y * 30);
     
     rotate(theta);
-    fill(10,80,140,160);
+    fill(10,80,140,200);
     noStroke()
     triangle(0,-tri*2,-tri,tri*2,tri,tri*2);
     resetMatrix();
@@ -48,31 +70,78 @@ class car {
     
   }
 
+  edge(){ 
+    if(dist(this.loc.x , this.loc.y , 0 ,0 ) > safe_r){
+      var desired  = p5.Vector.sub(createVector(0,0) , this.loc)
+      desired.normalize().mult(0.1)
+      this.acc.add(desired)
+    }
+  }
+
 }
 
 var CAR ;
-
+let n  ; 
 function setup() {
   // put setup code here
   createCanvas(600,600);
+  r= int(r);
+  n = 0
+  A_t = float(At_s.value) / 20
   CAR = new car(r,0);
 }
 
 
 function draw() {
   // put drawing code here
-  background(255);
+
+  background(250);
   translate(width/2 , height/2);
-  for( let i = 0.0 ; i <= TWO_PI ; i+=0.1 ){
-    var x = sin(i) * r ; 
-    var y = cos(i) * r ; 
-    strokeWeight(5);
+  strokeWeight(5);
+  for( let i = 0.0 ; i <= TWO_PI ; i+=20/r){
     stroke(125);
+    var y = sin(i) * safe_r ; 
+    var x = cos(i) * safe_r; 
     point(x,y );
 
+    stroke(80,120,240,100)
+    var y = sin(i) * r ; 
+    var x = cos(i) * r; 
+    point(x,y );
   }
-  CAR.update();
-  CAR.draw();
 
+
+  CAR.update();
+
+  strokeWeight(1)
+  stroke(125);
+  line(0,0,CAR.loc.x , CAR.loc.y)
+  CAR.draw();
   
+}
+
+Rslider.onchange = ()=>{
+    r= int(Rslider.value);
+    safe_r = r + 80 
+    CAR.loc = createVector(r,0);
+    CAR.vel = createVector(0,4);
+}
+
+velSlider.onchange = ()=>{
+  let vel = int(velSlider.value); 
+  CAR.loc = createVector(r,0);
+  CAR.vel = createVector(0,vel);
+  CAR.maxspeed = vel ;
+}
+
+
+
+At_s.onchange = ()=>{
+  A_t = float(At_s.value) /20
+}
+
+irr_check.onchange = ()=>{
+  irregular_acc = !irregular_acc
+  CAR.loc = createVector(r,0);
+  CAR.vel = createVector(0,4);
 }
